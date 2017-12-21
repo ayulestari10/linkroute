7dcfc836451416f3a895700e0f8ca97e2f61d85b
sarirahman <trikurniasari05@gmail.com>
2017-12-21 10:59:21 +0700

    ubah

--- application/views/linkroute.php
+++ application/views/linkroute.php
@@ -15,6 +15,12 @@
                         </div>
                         <!-- /.panel-heading -->
                         <div class="panel-body">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <?= $this->session->flashdata('msg'); ?>
+                                </div>
+                            </div>
+
                             <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
                                 <thead>
                                     <tr>
@@ -44,7 +50,7 @@
                                         <td><?= $row->HOP_ID_DETAIL ?></td>
                                         <td>
                                             <a href="<?= base_url('Home/edit_linkroute/' . $row->id)  ?>" class="btn btn-info btn-circle"><i class="fa fa-pencil-square-o"></i></a>
-                                            <button type="button" class="btn btn-danger btn-circle"><i class="fa fa-trash"></i></button>
+                                            <button onclick="delete_linkroute(<?= $row->id ?>)" class="btn btn-danger btn-circle"><i class="fa fa-trash"></i></button>
                                         </td>
                                     </tr>
                                 <?php endforeach; ?>
@@ -65,3 +71,19 @@
 
     </div>
     <!-- /#wrapper -->
+
+    <script type="text/javascript">
+        function delete_linkroute(id) {
+            $.ajax({
+                url: '<?= base_url('home/linkroute') ?>',
+                type: 'POST',
+                data: {
+                    id: id,
+                    delete: true
+                },
+                success: function() {
+                    window.location = '<?= base_url('home/linkroute') ?>';
+                }
+            });
+        }
+    </script>
